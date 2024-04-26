1f12c266c173cd3c224476853bdbf6bdecba7f4c
Fontana <162226403+fontanaGu@users.noreply.github.com>
2024-04-26 17:40:48 -0300

    Arrumadinha de leve

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -1,46 +1,44 @@
-﻿namespace Triangulos.ConsoleApp
+﻿namespace Triangulos.ConsoleApp;
 
-    // Dados três valores X, Y, Z, verifiquem se eles podem ser os comprimentos dos lados de um triângulo e se forem escrever uma mensagem informando se é um triângulo eqüilátero, isósceles ou escaleno.
-    // Observações:
-    // O comprimento de um lado do triângulo é sempre menor do que a soma dos outros dois.
-    // Eqüilátero: Todos os lados iguais
-    // Isósceles: Dois lados iguais
-    // Escaleno: Todos os lados diferentes
+// Dados três valores X, Y, Z, verifiquem se eles podem ser os comprimentos dos lados de um triângulo e se forem escrever uma mensagem informando se é um triângulo eqüilátero, isósceles ou escaleno.
+// Observações:
+// O comprimento de um lado do triângulo é sempre menor do que a soma dos outros dois.
+// Eqüilátero: Todos os lados iguais
+// Isósceles: Dois lados iguais
+// Escaleno: Todos os lados diferentes
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-          Triangulo triangulo1 = new Triangulo(3,3,3);
-            triangulo1.X = 3;
-            triangulo1.Y = 3;
-            triangulo1.Z = 3;
+        var triangulo1 = new Triangulo.Triangulo(3, 3, 3);
+        triangulo1.X = 3;
+        triangulo1.Y = 3;
+        triangulo1.Z = 3;
 
-            Console.WriteLine(triangulo1.VerificarTriangulo());
+        Console.WriteLine(triangulo1.VerificarTriangulo());
 
-            Triangulo triangulo2 = new Triangulo(3,3,4);
-            triangulo2.X = 3;
-            triangulo2.Y = 3;
-            triangulo2.Z = 4;
+        var triangulo2 = new Triangulo.Triangulo(3, 3, 4);
+        triangulo2.X = 3;
+        triangulo2.Y = 3;
+        triangulo2.Z = 4;
 
-            Console.WriteLine(triangulo2.VerificarTriangulo());
+        Console.WriteLine(triangulo2.VerificarTriangulo());
 
-            Triangulo triangulo3 = new Triangulo(3,4,5);
-            triangulo3.X = 3;
-            triangulo3.Y = 4;
-            triangulo3.Z = 5;
+        var triangulo3 = new Triangulo.Triangulo(3, 4, 5);
+        triangulo3.X = 3;
+        triangulo3.Y = 4;
+        triangulo3.Z = 5;
 
-            Console.WriteLine(triangulo3.VerificarTriangulo());
+        Console.WriteLine(triangulo3.VerificarTriangulo());
 
-            Triangulo triangulo4 = new Triangulo(3,4,8);
-            triangulo4.X = 3;
-            triangulo4.Y = 4;
-            triangulo4.Z = 8;
+        var triangulo4 = new Triangulo.Triangulo(3, 4, 8);
+        triangulo4.X = 3;
+        triangulo4.Y = 4;
+        triangulo4.Z = 8;
 
-            Console.WriteLine(triangulo4.VerificarTriangulo());
+        Console.WriteLine(triangulo4.VerificarTriangulo());
 
-            Console.ReadKey();
-
-        }
+        Console.ReadKey();
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo/Triangulo.cs
@@ -0,0 +1,29 @@
+﻿namespace Triangulos.ConsoleApp.Triangulo;
+
+public class Triangulo
+{
+    public Triangulo(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+    public string VerificarTriangulo()
+    {
+        if (X < Y + Z && Y < X + Z && Z < X + Y)
+        {
+            if (X == Y && Y == Z)
+                return "Triângulo Equilátero";
+            if (X == Y || X == Z || Y == Z)
+                return "Triângulo Isósceles";
+            return "Triângulo Escaleno";
+        }
+
+        return "Não é um triângulo";
+    }
+}
